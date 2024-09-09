5f2a9ce6f21a0682eac28723307d8f4b3444169c
IM-GYURI <kgulr0517@gmail.com>
2024-09-09 19:13:58 +0900

    feat : initial setting

---
+++ src/main/java/plannery/flora/FloraApplication.java
@@ -0,0 +1,13 @@
+package plannery.flora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloraApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FloraApplication.class, args);
+  }
+
+}
---
+++ src/test/java/plannery/flora/FloraApplicationTests.java
@@ -0,0 +1,13 @@
+package plannery.flora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloraApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
