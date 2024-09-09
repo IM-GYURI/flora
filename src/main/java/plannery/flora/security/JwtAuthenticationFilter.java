9bb397e36d49b146b7c63b7998d954f61f4c9beb
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 02:06:37 +0900

    feat : add JwtAuthenticationFilter
    doFilterInternal()
    - 요청 헤더에서 JWT token 추출
    - 추출한 토큰이 유효한 경우 : Authentication 객체 생성 및 SecurityContextHolder에 설정
    - 추출한 토큰이 유효하지 않은 경우 : INVALID_TOKEN
    
    resolveToken()
    - 요청 헤더에서 Authorization 필드를 가져와 "Bearer "로 시작하는지 확인
    
    setErrorResponse()
    - 오류 응답을 Json 형식으로 설정하고 클라이언트에 반환
    - 오류 메세지와 상태 코드는 ErrorCode에서 가져옴

---
+++ src/main/java/plannery/flora/security/JwtAuthenticationFilter.java
@@ -0,0 +1,70 @@
+package plannery.flora.security;
+
+import static plannery.flora.exception.ErrorCode.INVALID_TOKEN;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import plannery.flora.exception.CustomException;
+import plannery.flora.exception.ErrorCode;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+  private static final String TOKEN_HEADER = "Authorization";
+  private static final String TOKEN_PREFIX = "Bearer ";
+
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String token = resolveToken(request);
+
+    if (StringUtils.hasText(token)) {
+      try {
+        if (jwtTokenProvider.validateToken(token)) {
+          Authentication authentication = jwtTokenProvider.getAuthentication(token);
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+          throw new CustomException(INVALID_TOKEN);
+        }
+      } catch (CustomException e) {
+        setErrorResponse(response, e.getErrorCode());
+        return;
+      } catch (Exception e) {
+        throw e;
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String resolveToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(TOKEN_HEADER);
+
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(TOKEN_PREFIX)) {
+      return bearerToken.substring(TOKEN_PREFIX.length());
+    }
+
+    return null;
+  }
+
+  private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode)
+      throws IOException {
+    response.setContentType("application/json;charset=UTF-8");
+    response.setStatus(errorCode.getStatus());
+    response.getWriter().write(errorCode.getMessage());
+  }
+}
