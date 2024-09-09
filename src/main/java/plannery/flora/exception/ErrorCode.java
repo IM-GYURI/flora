98c665742862be9f7977a0161cbbd95a0622c3fc
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 03:12:00 +0900

    feat : add ErrorCode
    - BLACKLIST_TOKEN_ADD_FAILED

--- src/main/java/plannery/flora/exception/ErrorCode.java
+++ src/main/java/plannery/flora/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
   MEMBER_NOT_FOUND(404, "가입된 회원을 찾을 수 없습니다."),
   INVALID_MEMBER_ID(401, "유효하지 않은 회원 아이디입니다."),
   PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
-  INVALID_TOKEN(401, "유효하지 않은 토큰입니다.");
+  INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+  BLACKLIST_TOKEN_ADD_FAILED(500, "블랙리스트 토큰 추가에 실패했습니다.");
 
   private final int status;
   private final String message;
