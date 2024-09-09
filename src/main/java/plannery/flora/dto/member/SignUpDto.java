c1923b6e96cc484c495d7af4b05fa42a042100c8
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 02:17:58 +0900

    feat : add SignUpDto
    - email
    - password

---
+++ src/main/java/plannery/flora/dto/member/SignUpDto.java
@@ -0,0 +1,24 @@
+package plannery.flora.dto.member;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDto {
+
+  @NotBlank(message = "이메일은 필수 입력값입니다.")
+  @Email(message = "이메일 형식에 맞지 않습니다.")
+  private String email;
+
+  @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+  private String password;
+}
