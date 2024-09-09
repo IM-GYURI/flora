7f778dedefd56f0979b8173d0916a64cb65b6d6a
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 02:00:41 +0900

    feat : add @EnableJpaAuditing

--- src/main/java/plannery/flora/FloraApplication.java
+++ src/main/java/plannery/flora/FloraApplication.java
@@ -2,8 +2,10 @@ package plannery.flora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FloraApplication {
 
   public static void main(String[] args) {
