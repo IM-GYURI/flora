ecf4be8e88e76363abe647f2b6816fa895f978a0
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 02:14:54 +0900

    feat : add MemberEntity
    - id
    - email
    - password
    - role

---
+++ src/main/java/plannery/flora/entity/MemberEntity.java
@@ -0,0 +1,38 @@
+package plannery.flora.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import plannery.flora.enums.UserRole;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member")
+public class MemberEntity extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private UserRole role;
+}
