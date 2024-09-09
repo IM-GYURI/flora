9db0d83fb8916eefe0489fe4aaf12ea3289f17d8
IM-GYURI <kgulr0517@gmail.com>
2024-09-10 02:18:21 +0900

    feat : add SwaggerConfig
    Swagger 설정 Configuration

---
+++ src/main/java/plannery/flora/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package plannery.flora.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Flora")
+            .description("Project Flora Swagger API")
+            .version("1.0")
+        );
+  }
+}
