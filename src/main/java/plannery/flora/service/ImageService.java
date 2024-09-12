789b52dd5da702a552404a0aeeebaca564b3fc0e
IM-GYURI <kgulr0517@gmail.com>
2024-09-12 21:06:36 +0900

    feat : add ImageService
    checkUserDetails()를 SecurityUtil.validateUserDetails()로 변경

--- src/main/java/plannery/flora/service/ImageService.java
+++ src/main/java/plannery/flora/service/ImageService.java
@@ -4,7 +4,6 @@ import static plannery.flora.enums.ImageType.IMAGE_GALLERY;
 import static plannery.flora.enums.ImageType.IMAGE_PROFILE;
 import static plannery.flora.exception.ErrorCode.IMAGE_NOT_FOUND;
 import static plannery.flora.exception.ErrorCode.MEMBER_NOT_FOUND;
-import static plannery.flora.exception.ErrorCode.NO_AUTHORITY;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import plannery.flora.component.S3ImageUpload;
+import plannery.flora.component.SecurityUtils;
 import plannery.flora.entity.ImageEntity;
 import plannery.flora.entity.MemberEntity;
 import plannery.flora.enums.ImageType;
@@ -28,6 +28,7 @@ public class ImageService {
   private final S3ImageUpload s3ImageUpload;
   private final MemberRepository memberRepository;
   private final ImageRepository imageRepository;
+  private final SecurityUtils securityUtils;
 
   @Value("${app.default.profile.url}")
   private String defaultProfileUrl;
@@ -74,12 +75,11 @@ public class ImageService {
    */
   public String uploadImage(UserDetails userDetails, MultipartFile file, Long memberId,
       ImageType imageType) {
-    checkUserDetails(userDetails, memberId);
+    MemberEntity member = securityUtils.validateUserDetails(userDetails, memberId);
 
     ImageEntity imageEntity = imageRepository.findByMemberIdAndImageType(memberId, imageType)
         .orElseGet(() -> ImageEntity.builder()
-            .member(memberRepository.findById(memberId)
-                .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND)))
+            .member(member)
             .imageType(imageType)
             .build());
 
@@ -108,7 +108,7 @@ public class ImageService {
    * @return 이미지 URL
    */
   public String getImage(UserDetails userDetails, Long memberId, ImageType imageType) {
-    checkUserDetails(userDetails, memberId);
+    MemberEntity member = securityUtils.validateUserDetails(userDetails, memberId);
 
     return imageRepository.findByMemberIdAndImageType(memberId, imageType)
         .map(ImageEntity::getImageUrl)
@@ -122,7 +122,7 @@ public class ImageService {
    * @param imageType 이미지 타입 : IMAGE_PROFILE, IMAGE_GALLERY
    */
   public void deleteImage(UserDetails userDetails, Long memberId, ImageType imageType) {
-    checkUserDetails(userDetails, memberId);
+    MemberEntity member = securityUtils.validateUserDetails(userDetails, memberId);
 
     ImageEntity imageEntity = imageRepository.findByMemberIdAndImageType(memberId, imageType)
         .orElseThrow(() -> new CustomException(IMAGE_NOT_FOUND));
@@ -139,15 +139,6 @@ public class ImageService {
         imageEntity.updateImage(defaultGalleryUrl);
         imageRepository.save(imageEntity);
       }
-    }
-  }
-
-  private void checkUserDetails(UserDetails userDetails, Long memberId) {
-    MemberEntity member = memberRepository.findByEmail(userDetails.getUsername())
-        .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
-
-    if (!member.getId().equals(memberId)) {
-      throw new CustomException(NO_AUTHORITY);
     }
   }
 }
