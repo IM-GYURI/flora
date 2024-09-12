5f59b8478b13cda21eebf616c9b87e25d1b59e2e
IM-GYURI <kgulr0517@gmail.com>
2024-09-12 19:08:30 +0900

    feat : add ResponseMessage
    - SUCCESS_PASSWORD_CHANGE
    - SUCCESS_SEND_PASSWORD_CHANGE
    - SUCCESS_MEMBER_DELETE

--- src/main/java/plannery/flora/enums/ResponseMessage.java
+++ src/main/java/plannery/flora/enums/ResponseMessage.java
@@ -9,7 +9,10 @@ public enum ResponseMessage {
   SUCCESS_SIGNUP("회원가입 완료"),
   SUCCESS_SIGNOUT("로그아웃 완료"),
   NO_IMAGE_FILE("이미지 파일 부재"),
-  SUCCESS_IMAGE_DELETE("이미지 파일 삭제 완료");
+  SUCCESS_IMAGE_DELETE("이미지 파일 삭제 완료"),
+  SUCCESS_PASSWORD_CHANGE("비밀번호 변경 완료"),
+  SUCCESS_SEND_PASSWORD_CHANGE("비밀번호 변경 링크 전송 완료"),
+  SUCCESS_MEMBER_DELETE("회원 탈퇴 완료");
 
   private final String message;
 }
