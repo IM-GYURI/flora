a5a86e4a8bca0065dad9063aa642e66edf86986d
IM-GYURI <kgulr0517@gmail.com>
2024-09-19 00:40:42 +0900

    fix : fix SignUpDto
    - fix regexp of password

--- src/main/java/plannery/flora/dto/member/SignUpDto.java
+++ src/main/java/plannery/flora/dto/member/SignUpDto.java
@@ -19,6 +19,9 @@ public class SignUpDto {
   private String email;
 
   @NotBlank(message = "비밀번호는 필수 입력값입니다.")
-  @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+  @Pattern(
+      regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|])[A-Za-z\\d~!@#$%^&*()+|]{8,16}$",
+      message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 각각 하나씩 포함해야 합니다."
+  )
   private String password;
 }
