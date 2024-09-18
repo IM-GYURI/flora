6ce2add5e549a504aa821bac483b77b18b26370e
IM-GYURI <kgulr0517@gmail.com>
2024-09-19 00:39:08 +0900

    fix : fix PasswordChangeDto
    - change Dto name from ChangePasswordDto to PasswordChangeDto
    - fix regexp of newPassword

--- src/main/java/plannery/flora/dto/member/PasswordChangeDto.java
+++ src/main/java/plannery/flora/dto/member/PasswordChangeDto.java
@@ -11,14 +11,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChangePasswordDto {
+public class PasswordChangeDto {
 
   @NotBlank(message = "현재 비밀번호는 필수 입력값입니다.")
   private String oldPassword;
 
   @NotBlank(message = "새 비밀번호는 필수 입력값입니다.")
   @Pattern(
-      regexp = "^(?=^.{8,16}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{\":;'?/<>.,])(?!.*\\s).*$",
-      message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+      regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|])[A-Za-z\\d~!@#$%^&*()+|]{8,16}$",
+      message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 각각 하나씩 포함해야 합니다."
+  )
   private String newPassword;
 }
