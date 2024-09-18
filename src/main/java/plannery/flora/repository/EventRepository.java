d7bfc18cb086c21f7ae3812fb30700ec18af899d
IM-GYURI <kgulr0517@gmail.com>
2024-09-19 00:49:04 +0900

    feat : add EventRepository
    - findAllByMemberIdAndStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqual()
    - findEventsForMemberWithinMonth()
    - findDDayEventsByMemberId()

---
+++ src/main/java/plannery/flora/repository/EventRepository.java
@@ -0,0 +1,26 @@
+package plannery.flora.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import plannery.flora.entity.EventEntity;
+
+@Repository
+public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+  List<EventEntity> findAllByMemberIdAndStartDateTimeLessThanEqualAndEndDateTimeGreaterThanEqual(
+      Long memberId, LocalDateTime startOfDay, LocalDateTime endOfDay);
+
+  @Query("SELECT e FROM EventEntity e WHERE e.member.id = :memberId " +
+      "AND (e.startDateTime <= :endDate AND e.endDateTime >= :startDate)")
+  List<EventEntity> findEventsForMemberWithinMonth(@Param("memberId") Long memberId,
+      @Param("startDate") LocalDateTime startDate,
+      @Param("endDate") LocalDateTime endDate);
+
+  @Query("SELECT e FROM EventEntity e WHERE e.member.id = :memberId AND e.isDDay = true AND e.startDateTime >= :todayStartOfDay")
+  List<EventEntity> findDDayEventsByMemberId(@Param("memberId") Long memberId,
+      @Param("todayStartOfDay") LocalDateTime todayStartOfDay);
+}
