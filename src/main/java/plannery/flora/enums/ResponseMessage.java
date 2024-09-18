8b0872caee894a34858bc61c4724ea78b1f07bb1
IM-GYURI <kgulr0517@gmail.com>
2024-09-19 00:39:31 +0900

    fix : fix ResponseMessage
    - SUCCESS_NOTIFICATION_CREATE
    - SUCCESS_EVENT_CREATE
    - SUCCESS_EVENT_UPDATE
    - SUCCESS_EVENT_DELETE

--- src/main/java/plannery/flora/enums/ResponseMessage.java
+++ src/main/java/plannery/flora/enums/ResponseMessage.java
@@ -18,7 +18,11 @@ public enum ResponseMessage {
   SUCCESS_PROMISE_DELETE("다짐 삭제 완료"),
   SUCCESS_DIARY_CREATE("일기 생성 완료"),
   SUCCESS_DIARY_UPDATE("일기 수정 완료"),
-  SUCCESS_DIARY_DELETE("일기 삭제 완료");
+  SUCCESS_DIARY_DELETE("일기 삭제 완료"),
+  SUCCESS_NOTIFICATION_CREATE("알림 생성 완료"),
+  SUCCESS_EVENT_CREATE("이벤트 생성 완료"),
+  SUCCESS_EVENT_UPDATE("이벤트 수정 완료"),
+  SUCCESS_EVENT_DELETE("이벤트 삭제 완료");
 
   private final String message;
 }
