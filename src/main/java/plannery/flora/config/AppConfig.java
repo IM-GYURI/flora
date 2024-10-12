f43d4946ece8b4bf195023125463077569d5a82d
IM-GYURI <kgulr0517@gmail.com>
2024-10-13 03:28:37 +0900

    feat : add AppConfig
    - restTemplate() 추가

--- src/main/java/plannery/flora/config/AppConfig.java
+++ src/main/java/plannery/flora/config/AppConfig.java
@@ -3,7 +3,9 @@ package plannery.flora.config;
 import jakarta.annotation.PostConstruct;
 import java.util.TimeZone;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Configuration
@@ -13,5 +15,10 @@ public class AppConfig {
   public void init() {
     TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     log.info("기본 TimeZone 설정 = Asia/Seoul");
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
   }
 }
