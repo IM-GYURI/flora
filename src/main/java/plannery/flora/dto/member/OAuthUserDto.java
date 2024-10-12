bc5f4f94028ba60c16ee237ec222a6fc1eb7bee2
IM-GYURI <kgulr0517@gmail.com>
2024-10-13 03:34:08 +0900

    feat : add OAuthUserDto
    - email

---
+++ src/main/java/plannery/flora/dto/member/OAuthUserDto.java
@@ -0,0 +1,15 @@
+package plannery.flora.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OAuthUserDto {
+
+  private String email;
+}
